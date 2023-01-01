2a935d5763d7f4ae14d36670b2c1e04d884ee927
Simba-ExC <117112169+Simba-ExC@users.noreply.github.com>
2023-01-01 16:30:17 +0300

    Add files via upload

---
+++ Multi-file_projects_and_libraries/01/Mathematical functions/Mathematical functions.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "math_power.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "");
+
+    int a = 0;
+    int b = 0;
+    int chose = 0 ;
+    int equally = 0;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> a;
+    std::cout << "Введите второе число: ";
+    std::cin >> b;
+    
+    
+    std::cout << "\nВыберите операцию (1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень): ";
+    std::cin >> chose;
+    if (chose == 1)
+    {
+        std::cout << " \n " << a << " сложить с " << b << " = "  << addition(a, b);
+    }
+    else if (chose == 2)
+    {
+        std::cout << " \n " << a << " вычисть " << b << " = " << subtraction(a, b);
+    }
+    else if (chose == 3)
+    {
+        std::cout << " \n " << a << " умножить на " << b << " = " << multiplication(a, b);
+    }
+    else if (chose == 4)
+    {
+        std::cout << " \n " << a << " поделить на  "<< b << " = "<< division(a, b);
+    }
+    else if (chose == 5)
+    {
+        std::cout << " \n " << a << " в степени " << b << " = " << exponentiation(a, b);
+    }
+    else 
+    {
+        std::cout << "Пункта", chose, " не существует";
+    }
+}
---
+++ Multi-file_projects_and_libraries/01/Mathematical functions/math_power.cpp
@@ -0,0 +1,23 @@
+#include "math_power.h"
+
+
+int addition(int a, int b)
+{
+	return a + b;
+}
+int subtraction(int a, int b)
+{
+	return a - b;
+}
+int multiplication(int a, int b)
+{
+	return a * b;
+}
+int division(int a, int b)
+{
+	return a / b;
+}
+int exponentiation(int a, int b)
+{
+	return a ^ b;
+}
